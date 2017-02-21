016e8972166f9ce626e1b6cdf8d56c96a6e50b49
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-02-20 19:55:24 -0800

    fixed add page to use camel case

--- tools/cli/add_page.js
+++ tools/cli/add_page.js
@@ -6,7 +6,7 @@ const shell = require('shelljs');
 const helpers = require('./helpers');
 
 const args = process.argv;
-const pageName = _.kebabCase(args[2]);
+const pageName = _.camelCase(args[2]);
 
 if (!pageName) {
   console.log('Error: Please set the page name');
@@ -16,7 +16,7 @@ if (!pageName) {
 const context = {
   PAGE_NAME: _.upperFirst(_.camelCase(pageName)),
   CAMEL_PAGE_NAME: _.camelCase(pageName),
-  KEBAB_PAGE_NAME: _.kebabCase(pageName),
+  SNAKE_PAGE_NAME: _.snakeCase(pageName),
   CAPS_PAGE_NAME: pageName.toUpperCase(),
   LOWER_PAGE_NAME: pageName.toLowerCase(),
   UPPER_SNAKE_PAGE_NAME: _.kebabCase(pageName).toUpperCase(),
@@ -27,15 +27,16 @@ const myContext = {
   Template: _.upperFirst(_.camelCase(pageName)),
   TEMPLATE: pageName.toUpperCase(),
   template: pageName.toLowerCase(),
-  templateKebab: _.kebabCase(pageName)
+  templateKebab: _.kebabCase(pageName),
+  templateCamel: _.camelCase(pageName),
 };
 
 const filesToSave = [];
 const toSave = helpers.getToSave(filesToSave);
 
-const targetDir = path.join(helpers.getProjectRoot(), `src/pages/${context.KEBAB_PAGE_NAME}`);
+const targetDir = path.join(helpers.getProjectRoot(), `src/pages/${context.CAMEL_PAGE_NAME}`);
 if (shell.test('-e', targetDir)) {
-  console.log(`Error: page name existed: ${context.KEBAB_PAGE_NAME}`);
+  console.log(`Error: page name existed: ${context.CAMEL_PAGE_NAME}`);
   process.exit(1);
 }
 
@@ -64,7 +65,7 @@ console.log('Add to root reducer.');
 targetPath = path.join(helpers.getProjectRoot(), 'src/common/rootReducer.js');
 lines = helpers.getLines(targetPath);
 i = helpers.lastLineIndex(lines, /^import /);
-lines.splice(i + 1, 0, `import ${context.CAMEL_PAGE_NAME}Reducer from '../pages/${context.KEBAB_PAGE_NAME}/redux/reducer';`);
+lines.splice(i + 1, 0, `import ${context.CAMEL_PAGE_NAME}Reducer from '../pages/${context.CAMEL_PAGE_NAME}/redux/reducer';`);
 i = helpers.lastLineIndex(lines, /^\}\);$/);
 lines.splice(i, 0, `  ${context.CAMEL_PAGE_NAME}: ${context.CAMEL_PAGE_NAME}Reducer,`);
 toSave(targetPath, lines);
@@ -74,7 +75,7 @@ console.log('Add to apply middleware.');
 targetPath = path.join(helpers.getProjectRoot(), 'src/common/store.js');
 lines = helpers.getLines(targetPath);
 i = helpers.lastLineIndex(lines, /^import /);
-lines.splice(i + 1, 0, `import ${context.CAMEL_PAGE_NAME}Middleware from '../pages/${context.KEBAB_PAGE_NAME}/redux/middleware';`);
+lines.splice(i + 1, 0, `import ${context.CAMEL_PAGE_NAME}Middleware from '../pages/${context.CAMEL_PAGE_NAME}/redux/middleware';`);
 i = helpers.lastLineIndex(lines, /^\)\(createStore\);$/);
 lines.splice(i, 0, `  ,${context.CAMEL_PAGE_NAME}Middleware`);
 toSave(targetPath, lines);
@@ -85,9 +86,9 @@ targetPath = path.join(helpers.getProjectRoot(), 'src/common/routeConfig.js');
 lines = helpers.getLines(targetPath);
 
 i = helpers.lastLineIndex(lines, /^import /);
-lines.splice(i + 1, 0, `import ${myContext.Template} from '../pages/auth/components/${myContext.Template}';`);
+lines.splice(i + 1, 0, `import ${myContext.templateCamel} from '../pages/${context.CAMEL_PAGE_NAME}/components';`);
 i = helpers.lineIndex(lines, 'path: \'*\'');
-lines.splice(i, 0, `    { path: '${myContext.templateKebab}', name: '${myContext.Template}', component: ${myContext.Template}},`);
+lines.splice(i, 0, `    { path: '${myContext.templateKebab}', name: '${myContext.Template}', component: ${myContext.templateCamel}},`);
 
 toSave(targetPath, lines);
 
@@ -99,8 +100,8 @@ helpers.saveFiles(filesToSave);
 console.log('Add page done: ', pageName);
 
 // Add page reducer test
-// shell.exec(`"${process.execPath}" ${__dirname}/add_reducer_test.js ${context.KEBAB_PAGE_NAME}`);
+// shell.exec(`"${process.execPath}" ${__dirname}/add_reducer_test.js ${context.CAMEL_PAGE_NAME}`);
 
 /* ==== Add sample page and test action ===== */
-// shell.exec(`"${process.execPath}" "${__dirname}/add_action.js" ${context.KEBAB_PAGE_NAME}/${context.KEBAB_PAGE_NAME}-test-action`);
-// shell.exec(`"${process.execPath}" "${__dirname}/add_page_file.js" ${context.KEBAB_PAGE_NAME}/default-page`);
+// shell.exec(`"${process.execPath}" "${__dirname}/add_action.js" ${context.CAMEL_PAGE_NAME}/${context.CAMEL_PAGE_NAME}-test-action`);
+// shell.exec(`"${process.execPath}" "${__dirname}/add_page_file.js" ${context.CAMEL_PAGE_NAME}/default-page`);
